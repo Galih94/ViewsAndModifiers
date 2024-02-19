ee99e28f36fbfac08ac0e7551515ff0171393485
galih <samudragalih94@gmail.com>
2024-02-19 21:25:05 +0700

    feat(UI): create `GridStack` a generic struct that conforms to `View` that has rows and columns

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,54 +7,28 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
-        
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.gray)
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
-
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.yellow
-            Text("Hello world!")
-                .titleStyle()
+        GridStack(rows: 4, columns: 5) { row, col in
+            Text("R:\(row) C:\(col)")
         }
-        .frame(maxWidth: 300, maxHeight: 300
-        )
-        .watermarked(with: "made by Galih")
     }
 }
 
